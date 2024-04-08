5caf6adc080ab0707d0f5f809bad449d58899f8f
okd08 <149041290+okd08@users.noreply.github.com>
2024-04-08 15:29:06 +0900

    herokuアップ用

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,5 +21,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap(); //Bootstrapでページネーションを使用
+
+        if (\App::environment(['production'])) {
+            \URL::forceScheme('https');
+        }
     }
 }
