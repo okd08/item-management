53e7b0729ce92078f3b6823cbedf6979541af91a
okd08 <149041290+okd08@users.noreply.github.com>
2024-04-21 17:02:30 +0900

    微修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,15 @@
 
 @section('content_header')
     <h1>ホーム</h1>
+    <div class="text-center mt-5">
+        <div class="btn-group mt-5">
+            <a class="btn btn-secondary" href="#" role="button">編集前のシステム</a>
+        </div>
+        <br>
+        <div class="btn-group mt-3">
+            <a class="btn btn-primary btn-lg" href="{{ route('items.index') }}" role="button">編集後のシステム</a>
+        </div>
+    </div>
 @stop
 
 @section('content')
